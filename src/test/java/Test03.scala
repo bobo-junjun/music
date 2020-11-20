b2114fe7073ccf5be4f2f71d747925adfd44795e
bobo-junjun <69711504+bobo-junjun@users.noreply.github.com>
2020-11-20 17:07:55 +0800

    音乐项目

--- src/test/java/Test03.scala
+++ src/test/java/Test03.scala
@@ -1,7 +1,10 @@
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
 import org.apache.spark.sql.Row
 import org.json.JSONObject
 
-import  scala.util.control.Breaks._
+import scala.util.control.Breaks._
 object Test03 {
   def main(args: Array[String]): Unit = {
 //    breakable{
@@ -14,11 +17,23 @@ object Test03 {
 //      }
 //    }
 
+//
+//
+//    for(i <- 1 to 3 if i == 2){
+//      println(i)
+//    }
 
-
-    for(i <- 1 to 3 if i == 2){
-      println(i)
+    def getDataTime(times:String,num:Int): String ={
+      val format = new SimpleDateFormat(times)
+      val date = format.parse(times)
+      val calendar = Calendar.getInstance()
+      calendar.setTime(date)
+      calendar.add(Calendar.DAY_OF_MONTH,-num)
+      format.format(calendar.getTime)
     }
+
+
+
 
   }
 }
